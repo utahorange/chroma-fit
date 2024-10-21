8276c00383514a0af09da91459da206d3999fad7
YashAgarwal06 <emailyashagarwal@gmail.com>
2024-10-20 22:59:37 -0700

    added basic gradio image upload to main

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
+import gradio as gr
 
+def upload_image(image):
+    return image
 
+# Create Gradio interface
+interface = gr.Interface(
+    fn=upload_image,  # The function to process the uploaded image
+    inputs=gr.Image(type='filepath'),  # Input is an image file
+    outputs="image",  # Output is also displayed as an image
+    title="Simple Image Uploader",
+    description="Upload an image file and display it below."
+)
 
-def detect_faces(path):
-    """Detects faces in an image."""
-    from google.cloud import vision
+# Launch the interface
+interface.launch()
 
-    client = vision.ImageAnnotatorClient()
-
-    with open(path, "rb") as image_file:
-        content = image_file.read()
-
-    image = vision.Image(content=content)
-
-    response = client.face_detection(image=image)
-    faces = response.face_annotations
-
-    # Names of likelihood from google.cloud.vision.enums
-    likelihood_name = (
-        "UNKNOWN",
-        "VERY_UNLIKELY",
-        "UNLIKELY",
-        "POSSIBLE",
-        "LIKELY",
-        "VERY_LIKELY",
-    )
-    print("Faces:")
-
-    for face in faces:
-        print(f"anger: {likelihood_name[face.anger_likelihood]}")
-        print(f"joy: {likelihood_name[face.joy_likelihood]}")
-        print(f"surprise: {likelihood_name[face.surprise_likelihood]}")
-
-        vertices = [
-            f"({vertex.x},{vertex.y})" for vertex in face.bounding_poly.vertices
-        ]
-
-        print("face bounds: {}".format(",".join(vertices)))
-
-    if response.error.message:
-        raise Exception(
-            "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(response.error.message)
-        )
