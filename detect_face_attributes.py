ceb4a559da1ad1bb96020e170ee04bb50d26b76a
masonchoey <mason@choey.com>
2024-10-22 12:03:49 -0700

    Update detect_face_attributes.py
    put detect_face_attributes file inside a function so it doesn't call right when the program starts

--- detect_face_attributes.py
+++ detect_face_attributes.py
@@ -2,53 +2,54 @@ from google.cloud import vision
 from PIL import Image
 import os
 
-downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads") # this part will change based on how we implement our image upload later
-image_path = os.path.join(downloads_folder, "headshot.jpeg")
-# print(image_path)
+def detect_attributes(image_path):
+    # downloads_folder = os.path.join(os.path.expanduser("~"), "Desktop") # this part will change based on how we implement our image upload later
+    # image_path = os.path.join(downloads_folder, "myface")
+    # print(image_path)
 
-client = vision.ImageAnnotatorClient()
+    client = vision.ImageAnnotatorClient()
 
-with open(image_path, "rb") as image_file:
-    content = image_file.read()
+    with open(image_path, "rb") as image_file:
+        content = image_file.read()
 
-image = vision.Image(content=content)
+    image = vision.Image(content=content)
 
-response = client.face_detection(image=image)
-face = response.face_annotations
-face_landmarks = face[0].landmarks
+    response = client.face_detection(image=image)
+    face = response.face_annotations
+    face_landmarks = face[0].landmarks
 
-# print(face_landmarks)
+    # print(face_landmarks)
 
-# Names of likelihood from google.cloud.vision.enums
-likelihood_name = (
-    "UNKNOWN",
-    "VERY_UNLIKELY",
-    "UNLIKELY",
-    "POSSIBLE",
-    "LIKELY",
-    "VERY_LIKELY",
-)
-
-vertices = [
-    (vertex.x,vertex.y) for vertex in face[0].bounding_poly.vertices
-]
-
-# print("face bounds: {}".format(",".join(vertices)))
-print(vertices)
-original_img = Image.open(image_path)
-
-face_img = original_img.crop((vertices[0][0],vertices[1][1],vertices[1][0],vertices[2][1])) # (left, upper, right, lower) 
-# face_img.show()
-
-# ---- to work on here ---
-
-# right_eye_img = original_img.crop()
-# left_eye_img = original_img.crop()
-
-# print(face_landmarks)
-
-if response.error.message:
-    raise Exception(
-        "{}\nFor more info on error messages, check: "
-        "https://cloud.google.com/apis/design/errors".format(response.error.message)
+    # Names of likelihood from google.cloud.vision.enums
+    likelihood_name = (
+        "UNKNOWN",
+        "VERY_UNLIKELY",
+        "UNLIKELY",
+        "POSSIBLE",
+        "LIKELY",
+        "VERY_LIKELY",
     )
+
+    vertices = [
+        (vertex.x,vertex.y) for vertex in face[0].bounding_poly.vertices
+    ]
+
+    # print("face bounds: {}".format(",".join(vertices)))
+    print(vertices)
+    original_img = Image.open(image_path)
+
+    face_img = original_img.crop((vertices[0][0],vertices[1][1],vertices[1][0],vertices[2][1])) # (left, upper, right, lower)
+
+    # ---- to work on here ---
+
+    # right_eye_img = original_img.crop()
+    # left_eye_img = original_img.crop()
+
+    # print(face_landmarks)
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+    return(vertices)
