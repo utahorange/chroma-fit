2226d995ded7bc2d426e43a75a7bd9c5b9364aa8
masonchoey <mason@choey.com>
2024-10-23 21:35:48 -0700

    changed main.py to incorporate Yash's changes

--- main.py
+++ main.py
@@ -6,25 +6,28 @@ import os
 from PIL import Image
 from pillow_heif import register_heif_opener #type: ignore
 
-def upload_image(image): 
+def upload_image(face_image): 
     # image uploaded
-    if(not image.endswith("jpeg")):
-        if(image.endswith("heic")): 
-            register_heif_opener()
-            image.save(filepath, format='jpeg')
-        else:
-            print("file type is not jpeg")
-    '''
+    # if(not face_image.endswith("jpeg")):
+    #     if(face_image.endswith("heic")): 
+    #         register_heif_opener()
+    #         face_image.save(filepath, format='jpeg')
+    #     else:
+    #         print("file type is not jpeg")
+    
     # call detect_face_attributes, pictures of just face w white bg, just eyes, etc created
-    vertices = detect_face_attributes.detect_attributes(image)
-    # need to save image to filepath
-
+    # left_eye_image, right_eye_image, lip_image = detect_face_attributes(face_image)
+    
     # gcloud api dominant color determined for face, eyes, etc
-    primary_color = detect_primary_color.detect_properties(image, vertices)
+    # skin_primary_color = skin_tone.get_skin_tone(face_image)
+    # left_eye_primary_color = detect_primary_color.detect_properties(left_eye_image)
+    # right_eye_primary_color = detect_primary_color.detect_properties(right_eye_image)
+    # lip_primary_color = detect_primary_color.detect_properties(lip_image)
+
     # color analysis done w api's
-    return image
-    '''
-    return(skin_tone.get_skin_tone(image)) # returns a tuple of rgb color
+
+
+    return(face_image) # returns a tuple of rgb color
 
 # Create Gradio interface
 interface = gr.Interface(
